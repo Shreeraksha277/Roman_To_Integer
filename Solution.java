bc0fc2b71a662f013b3919e5b34e5020aa1c55d0
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-03-23 21:38:13 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,7 +3,7 @@ class Solution  {
          int total = 0;
  
         for (int i = 0; i < s.length(); i++) {
-            int value = getValue(s.charAt(i));
+             int value = getValue(s.charAt(i));
  
              if (i + 1 < s.length() && value < getValue(s.charAt(i + 1))) {
                 total -= value; // subtraction case
