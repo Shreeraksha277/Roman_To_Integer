2ad40e84ed2be580b8e1675464dc8d7b8600c34d
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-03-23 21:38:52 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,6 +1,6 @@
 class Solution  {
       public int romanToInt(String s)  {
-         int total = 0;
+          int total = 0;
  
          for (int i = 0; i < s.length(); i++) {
              int value = getValue(s.charAt(i));
