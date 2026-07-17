34592db5af558a24cb7ff472c5ed13c25c7292db
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-07-17 18:58:46 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -19,7 +19,7 @@
         switch (c) {
             case 'I': return 1;
             case 'V': return 5;
-            case 'X': return 10;
+             case 'X': return 10;
             case 'L': return 50;
             case 'C': return 100;
             case 'D': return 500;
